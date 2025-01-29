116e2f73e64096e8116164383e3d21d462fd5517
kwuraa <kwuraa@gmail.com>
2025-01-29 16:53:07 -0300

    integracao side bar/menu bar

--- src/public/views/appFunctions.js
+++ src/public/views/appFunctions.js
@@ -1,5 +1,6 @@
 const { ipcRenderer } = require("electron");
 const maxResBtn = document.getElementById("maxResBtn");
+
 const ipc = ipcRenderer;
 
 // MINIMIZE APP
@@ -32,4 +33,37 @@ ipc.on("isRestored", () => {
 // CLOSE APP
 closeBtn.addEventListener("click", () => {
   ipc.send("closeApp");
+});
+
+// TOGGLE MENU LEFT BAR
+const mySideBar = document.getElementById("mySideBar");
+const displayLogo = document.getElementById("displayLogo");
+const logo = document.getElementById("oldLogo");
+
+const buttons = document.querySelectorAll(" .buttons .unitButtons");
+
+var isLeftMenuIsOpened = false;
+
+showHideMenus.addEventListener("click", () => {
+  isLeftMenuIsOpened = !isLeftMenuIsOpened;
+
+  mySideBar.classList.toggle("opened", isLeftMenuIsOpened);
+  mySideBar.classList.toggle("closed", !isLeftMenuIsOpened);
+
+  showHideMenus.classList.toggle("icon-menu-open", isLeftMenuIsOpened);
+  showHideMenus.classList.toggle("icon-menu", !isLeftMenuIsOpened);
+
+  buttons.forEach((button) => {
+    const label = button.querySelector("label");
+    if (label) {
+      label.classList.toggle("labelOpen", isLeftMenuIsOpened);
+      label.classList.toggle("labelClose", !isLeftMenuIsOpened);
+    }
+  });
+
+  displayLogo.classList.toggle("opened", isLeftMenuIsOpened);
+  displayLogo.classList.toggle("closed", !isLeftMenuIsOpened);
+
+  logo.classList.toggle("logoOpen", isLeftMenuIsOpened);
+  logo.classList.toggle("logoClose", !isLeftMenuIsOpened);
 });

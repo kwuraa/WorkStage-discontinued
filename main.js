62c42312eadf9a8dbe39669306806f9c265e966d
kwuraa <kwuraa@gmail.com>
2025-01-29 01:58:58 -0300

    implementando logica para funcionalidades dos botões da title bar

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
 console.log("Rodando ...");
 
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require("node:path");
+const ipc = ipcMain;
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
+    minWidth: 940,
+    minHeight: 560,
+    frame: false,
     // titleBarStyle: "hidden",
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: true,
+      contextIsolation: false,
       devTools: true,
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
   win.loadFile("./src/public/views/index.html");
+
+  // MINIMIZE APP
+  ipc.on("minimizeApp", () => {
+    win.minimize();
+  });
+
+  // MAXIMIZE RESTORE APP
+  ipc.on("maximizeRestoreApp", () => {
+    if (win.isMaximized()) {
+      win.restore();
+    } else {
+      win.maximize();
+    }
+  });
+
+  // Check Maximized
+  win.on("maximize", () => {
+    win.webContents.send("isMaximized");
+  });
+
+  // Check Restored
+  win.on("unmaximize", () => {
+    win.webContents.send("isRestored");
+  });
+
+  // CLOSE APP
+  ipc.on("closeApp", () => {
+    win.close();
+  });
 };
 
 app.whenReady().then(() => {
--- src/public/views/render.js
+++
@@ -1,1 +0,0 @@
-console.log(`Electron: ${api.verElectron()}`);
